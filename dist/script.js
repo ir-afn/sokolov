18ec501ac28cd69eebf4010b73c37fa8a28c2310
ir-afn <thunderburker@gmail.com>
2022-01-21 16:59:37 +0300

    add: gulpfile

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+console.log("hey");const checkbox=document.getElementById(check);checkbox.addEventListener("submit",(function(e){checkbox.classList.add("hidden")}));
+//# sourceMappingURL=script.js.map
